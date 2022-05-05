d6f49ae2f3e51d48a9d63dbb5e5937fa85bb19e6
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-05-05 15:22:44 +0200

    TImeClient Time request button setup

---
+++ projects/Agario/Assets/Scripts/RequestServerTime.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestServerTime : MonoBehaviour
+{
+    public void SendRequest() {
+        Debug.Log("Hello");
+    }
+}
