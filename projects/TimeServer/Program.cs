0081766972a4246f37721cdc1ead8435e1b14ac3
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-05-05 14:40:39 +0200

    TimeServer program

--- projects/TimeServer/Program.cs
+++ projects/TimeServer/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeServer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 44);
+            tcpListener.Start();
+            
+            Console.WriteLine("Time Server waiting on connection");
+            
+            var tcpClient = tcpListener.AcceptTcpClient();
+            Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected.");
+
+            var stream = tcpClient.GetStream();
+            
+            stream.Write(Encoding.ASCII.GetBytes("Hello, this is Jasmines Time Server. The current time is: " + DateTime.Now));
+            stream.Close();
+            tcpListener.Stop();
+        }
+    }
+}
