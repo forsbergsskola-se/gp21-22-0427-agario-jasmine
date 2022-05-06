b3e3cd7305fdf776d6dc16e2830e6d1a6cf22f5e
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-05-06 19:00:44 +0200

    Request server time implementation
    Gets the current time and date from the local TimeServer

--- projects/Agario/Assets/Scripts/RequestServerTime.cs
+++ projects/Agario/Assets/Scripts/RequestServerTime.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 
 public class RequestServerTime : MonoBehaviour
 {
     public void SendRequest() {
-        Debug.Log("Hello");
+        TcpClient tcpClient = new TcpClient("127.0.0.1", 44);
+
+        var stream = tcpClient.GetStream();
+        byte[] bytes = new byte[tcpClient.ReceiveBufferSize];
+        stream.Read(bytes, 0, bytes.Length);
+        string info = Encoding.ASCII.GetString(bytes);
+        //Debug.Log(info);
+        //TODO: Output to text in Unity
     }
 }
