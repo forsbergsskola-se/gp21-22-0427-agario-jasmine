d7c9ab8a3ec5ef0c527a76bba7ca660e855c5c67
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-05-10 15:50:19 +0200

    TimeServer bug fixing
    No longer disconnects after one current time request

--- projects/TimeServer/Program.cs
+++ projects/TimeServer/Program.cs
@@ -8,19 +8,21 @@ namespace TimeServer
     class Program
     {
         static void Main(string[] args) {
+            
             TcpListener tcpListener = new TcpListener(IPAddress.Any, 44);
             tcpListener.Start();
             
             Console.WriteLine("Time Server waiting on connection");
             
-            var tcpClient = tcpListener.AcceptTcpClient();
-            Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected.");
-
-            var stream = tcpClient.GetStream();
+            while (true) {
+                var tcpClient = tcpListener.AcceptTcpClient();
+                Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected.");
+                
+                var stream = tcpClient.GetStream();
             
-            stream.Write(Encoding.ASCII.GetBytes("Hello, this is Jasmines Time Server. The current time is: " + DateTime.Now));
-            stream.Close();
-            tcpListener.Stop();
+                stream.Write(Encoding.ASCII.GetBytes("The current time is: " + DateTime.Now));
+                stream.Close();
+            }
         }
     }
 }
