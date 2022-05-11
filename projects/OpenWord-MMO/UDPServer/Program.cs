18a64940d0e247b550fdeb4e04da1be088d06ff7
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-05-12 00:01:11 +0200

    UDPServer terminal word display fixed

--- projects/OpenWord-MMO/UDPServer/Program.cs
+++ projects/OpenWord-MMO/UDPServer/Program.cs
@@ -6,18 +6,8 @@ using System.Text.RegularExpressions;
 
 public static class Program {
     static void Main(string[] arguments) {
-        // TODO: Setup server
-        // TODO: Connect to server & confirm
-        // TODO: Send word to server
-        // TODO: Validate word or send error message back
-        // TODO: Store words in meaning
-        // TODO: Send back meaning
-        // TODO: Allow another word
-
-        
         string wordSequence = "Word sequence is: ";
-        byte[] returnBytes;
-        
+
         var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 2222);
 
         while (true) {
@@ -26,7 +16,7 @@ public static class Program {
             IPEndPoint? clientEndpoint = default;
             
             var response = server.Receive(ref clientEndpoint);
-            string responseString = Encoding.ASCII.GetString(response);
+            string responseString = Encoding.ASCII.GetString(response).Trim();
 
             Regex regex = new Regex(@"^\S{0,20}$");
 
@@ -39,7 +29,7 @@ public static class Program {
             Console.WriteLine($"Packet received from: {clientEndpoint} saying: {responseString}");
             wordSequence += " " + responseString;
             
-            returnBytes = Encoding.ASCII.GetBytes(wordSequence);
+            byte[] returnBytes = Encoding.ASCII.GetBytes(wordSequence);
             server.Send(returnBytes, returnBytes.Length, clientEndpoint);
 
             server.Close();
