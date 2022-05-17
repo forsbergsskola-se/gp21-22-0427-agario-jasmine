0a21d896f2c772f0772404c1b1383dcd0924da4a
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-05-17 15:15:27 +0200

    Renaming variables in Main

--- projects/OpenWord-MMO/UDPServer/Program.cs
+++ projects/OpenWord-MMO/UDPServer/Program.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using UDPServer;
 
 public static class Program {
@@ -15,25 +14,25 @@ public static class Program {
         
         while (true) {
             IPEndPoint? clientEndpoint = default;
-            var response = server.Receive(ref clientEndpoint);
-            string responseString = Encoding.ASCII.GetString(response).Trim();
+            var responseBytes = server.Receive(ref clientEndpoint);
+            string response = Encoding.ASCII.GetString(responseBytes).Trim();
 
-            networkMessage.Result = WordValidator.ProcessWord(responseString);
+            networkMessage.Result = WordValidator.ProcessWord(response);
+            
             if (networkMessage.Result.Error != Error.None) {
                 SendNetworkMessage(networkMessage, server, clientEndpoint);
                 continue;
             }
 
-            Console.WriteLine($"Packet received from: {clientEndpoint} saying: {responseString}");
+            Console.WriteLine($"Packet received from: {clientEndpoint} saying: {response}");
             
-            networkMessage.Response += " " + responseString;
+            networkMessage.Response += " " + response;
             SendNetworkMessage(networkMessage, server, clientEndpoint);
         }
     }
 
     private static void SendNetworkMessage(NetworkMessage networkMessage, UdpClient? server, IPEndPoint? clientEndpoint) {
         string messageJson = JsonSerializer.Serialize(networkMessage);
-        Console.WriteLine(messageJson);
         byte[] messageBytes = Encoding.ASCII.GetBytes(messageJson);
         server?.Send(messageBytes, messageBytes.Length, clientEndpoint);
     }
